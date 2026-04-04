5fa18804fa193de681af21c181a9de1ea0e0860d
Vangel Trajkovski <Vangel.Trajkovski@netcetera.com>
2026-04-04 18:43:00 +0200

    Test

---
+++ libs/html-dom-js/1.1.2/lib/html-deprecated.js
@@ -0,0 +1,299 @@
+import { deprecatedElement } from './core/html.js';
+
+/** @import { type DeprecatedElementData } from './types/html.dom.js' */
+
+export { deprecatedElement as element };
+
+/**
+ * @deprecated
+ * @param {DeprecatedElementData<'acronym'>} data
+ * @returns {HTMLElementDeprecatedTagNameMap['acronym']}
+ */
+export function acronym(data) {
+  return deprecatedElement('acronym', data);
+}
+
+/**
+ * @deprecated
+ * @param {DeprecatedElementData<'applet'>} data
+ * @returns {HTMLElementDeprecatedTagNameMap['applet']}
+ */
+export function applet(data) {
+  return deprecatedElement('applet', data);
+}
+
+/**
+ * @deprecated
+ * @param {DeprecatedElementData<'basefont'>} data
+ * @returns {HTMLElementDeprecatedTagNameMap['basefont']}
+ */
+export function basefont(data) {
+  return deprecatedElement('basefont', data);
+}
+
+/**
+ * @deprecated
+ * @param {DeprecatedElementData<'bgsound'>} data
+ * @returns {HTMLElementDeprecatedTagNameMap['bgsound']}
+ */
+export function bgsound(data) {
+  return deprecatedElement('bgsound', data);
+}
+
+/**
+ * @deprecated
+ * @param {DeprecatedElementData<'big'>} data
+ * @returns {HTMLElementDeprecatedTagNameMap['big']}
+ */
+export function big(data) {
+  return deprecatedElement('big', data);
+}
+
+/**
+ * @deprecated
+ * @param {DeprecatedElementData<'blink'>} data
+ * @returns {HTMLElementDeprecatedTagNameMap['blink']}
+ */
+export function blink(data) {
+  return deprecatedElement('blink', data);
+}
+
+/**
+ * @deprecated
+ * @param {DeprecatedElementData<'center'>} data
+ * @returns {HTMLElementDeprecatedTagNameMap['center']}
+ */
+export function center(data) {
+  return deprecatedElement('center', data);
+}
+
+/**
+ * @deprecated
+ * @param {DeprecatedElementData<'dir'>} data
+ * @returns {HTMLElementDeprecatedTagNameMap['dir']}
+ */
+export function dir(data) {
+  return deprecatedElement('dir', data);
+}
+
+/**
+ * @deprecated
+ * @param {DeprecatedElementData<'font'>} data
+ * @returns {HTMLElementDeprecatedTagNameMap['font']}
+ */
+export function font(data) {
+  return deprecatedElement('font', data);
+}
+
+/**
+ * @deprecated
+ * @param {DeprecatedElementData<'frame'>} data
+ * @returns {HTMLElementDeprecatedTagNameMap['frame']}
+ */
+export function frame(data) {
+  return deprecatedElement('frame', data);
+}
+
+/**
+ * @deprecated
+ * @param {DeprecatedElementData<'frameset'>} data
+ * @returns {HTMLElementDeprecatedTagNameMap['frameset']}
+ */
+export function frameset(data) {
+  return deprecatedElement('frameset', data);
+}
+
+/**
+ * @deprecated
+ * @param {DeprecatedElementData<'isindex'>} data
+ * @returns {HTMLElementDeprecatedTagNameMap['isindex']}
+ */
+export function isindex(data) {
+  return deprecatedElement('isindex', data);
+}
+
+/**
+ * @deprecated
+ * @param {DeprecatedElementData<'keygen'>} data
+ * @returns {HTMLElementDeprecatedTagNameMap['keygen']}
+ */
+export function keygen(data) {
+  return deprecatedElement('keygen', data);
+}
+
+/**
+ * @deprecated
+ * @param {DeprecatedElementData<'listing'>} data
+ * @returns {HTMLElementDeprecatedTagNameMap['listing']}
+ */
+export function listing(data) {
+  return deprecatedElement('listing', data);
+}
+
+/**
+ * @deprecated
+ * @param {DeprecatedElementData<'marquee'>} data
+ * @returns {HTMLElementDeprecatedTagNameMap['marquee']}
+ */
+export function marquee(data) {
+  return deprecatedElement('marquee', data);
+}
+
+/**
+ * @deprecated
+ * @param {DeprecatedElementData<'menuitem'>} data
+ * @returns {HTMLElementDeprecatedTagNameMap['menuitem']}
+ */
+export function menuitem(data) {
+  return deprecatedElement('menuitem', data);
+}
+
+/**
+ * @deprecated
+ * @param {DeprecatedElementData<'multicol'>} data
+ * @returns {HTMLElementDeprecatedTagNameMap['multicol']}
+ */
+export function multicol(data) {
+  return deprecatedElement('multicol', data);
+}
+
+/**
+ * @deprecated
+ * @param {DeprecatedElementData<'nextid'>} data
+ * @returns {HTMLElementDeprecatedTagNameMap['nextid']}
+ */
+export function nextid(data) {
+  return deprecatedElement('nextid', data);
+}
+
+/**
+ * @deprecated
+ * @param {DeprecatedElementData<'nobr'>} data
+ * @returns {HTMLElementDeprecatedTagNameMap['nobr']}
+ */
+export function nobr(data) {
+  return deprecatedElement('nobr', data);
+}
+
+/**
+ * @deprecated
+ * @param {DeprecatedElementData<'noembed'>} data
+ * @returns {HTMLElementDeprecatedTagNameMap['noembed']}
+ */
+export function noembed(data) {
+  return deprecatedElement('noembed', data);
+}
+
+/**
+ * @deprecated
+ * @param {DeprecatedElementData<'noframes'>} data
+ * @returns {HTMLElementDeprecatedTagNameMap['noframes']}
+ */
+export function noframes(data) {
+  return deprecatedElement('noframes', data);
+}
+
+/**
+ * @deprecated
+ * @param {DeprecatedElementData<'param'>} data
+ * @returns {HTMLElementDeprecatedTagNameMap['param']}
+ */
+export function param(data) {
+  return deprecatedElement('param', data);
+}
+
+/**
+ * @deprecated
+ * @param {DeprecatedElementData<'plaintext'>} data
+ * @returns {HTMLElementDeprecatedTagNameMap['plaintext']}
+ */
+export function plaintext(data) {
+  return deprecatedElement('plaintext', data);
+}
+
+/**
+ * @deprecated
+ * @param {DeprecatedElementData<'rb'>} data
+ * @returns {HTMLElementDeprecatedTagNameMap['rb']}
+ */
+export function rb(data) {
+  return deprecatedElement('rb', data);
+}
+
+/**
+ * @deprecated
+ * @param {DeprecatedElementData<'rtc'>} data
+ * @returns {HTMLElementDeprecatedTagNameMap['rtc']}
+ */
+export function rtc(data) {
+  return deprecatedElement('rtc', data);
+}
+
+/**
+ * @deprecated
+ * @param {DeprecatedElementData<'spacer'>} data
+ * @returns {HTMLElementDeprecatedTagNameMap['spacer']}
+ */
+export function spacer(data) {
+  return deprecatedElement('spacer', data);
+}
+
+/**
+ * @deprecated
+ * @param {DeprecatedElementData<'strike'>} data
+ * @returns {HTMLElementDeprecatedTagNameMap['strike']}
+ */
+export function strike(data) {
+  return deprecatedElement('strike', data);
+}
+
+/**
+ * @deprecated
+ * @param {DeprecatedElementData<'tt'>} data
+ * @returns {HTMLElementDeprecatedTagNameMap['tt']}
+ */
+export function tt(data) {
+  return deprecatedElement('tt', data);
+}
+
+/**
+ * @deprecated
+ * @param {DeprecatedElementData<'xmp'>} data
+ * @returns {HTMLElementDeprecatedTagNameMap['xmp']}
+ */
+export function xmp(data) {
+  return deprecatedElement('xmp', data);
+}
+
+export default Object.freeze({
+  element: deprecatedElement,
+  acronym,
+  applet,
+  basefont,
+  bgsound,
+  big,
+  blink,
+  center,
+  dir,
+  font,
+  frame,
+  frameset,
+  isindex,
+  keygen,
+  listing,
+  marquee,
+  menuitem,
+  multicol,
+  nextid,
+  nobr,
+  noembed,
+  noframes,
+  param,
+  plaintext,
+  rb,
+  rtc,
+  spacer,
+  strike,
+  tt,
+  xmp,
+});
